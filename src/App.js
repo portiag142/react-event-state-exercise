7f24453640391c7fde3c024503b67c5eb9646550
Portia Greene <pd12300@yahoo.com>
2021-12-06 20:10:32 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// 1d
+import Exercise from "./exercise/Exercise";
+
+
+function App() {
+  return (
+    <div>
+      <Exercise />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/exercise/Exercise.js
@@ -0,0 +1,26 @@
+// 2a
+import { useState } from 'react';
+
+// 1a
+function Exercise() {
+  // 2c
+  const [num, setNum] = useState(1);
+
+  // 2d
+  const addNum = () => setNum(num + 1);
+
+  // 1c
+  return (
+    <section>
+      {/* 1e */}
+      <button onClick={() => alert("Congrats! You have clicked the button.")}>CLICK HERE</button>
+      {/* 2b & 2e */}
+      <button onClick={addNum}>STATE</button>
+      {/* 2f */}
+      <h1>{num}</h1>
+    </section>
+  );
+}
+
+// 1b
+export default Exercise;
